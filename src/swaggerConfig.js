aec2c20423a1c2c3eae5d10db4bd84fdb92a36f8
Bekzodmirzaaliyev <bekzodmirzaaliyev27@gmail.com>
2025-02-14 14:42:52 +0500

    ASD

--- src/swaggerConfig.js
+++ src/swaggerConfig.js
@@ -1,28 +1,25 @@
-const swaggerJsDoc = require('swagger-jsdoc');
-const path = require('path');
-
-const PORT = process.env.PORT || 5000;
+const swaggerJsDoc = require("swagger-jsdoc");
+const path = require("path");
 
 const swaggerOptions = {
   definition: {
-    openapi: '3.0.0',
+    openapi: "3.0.0",
     info: {
-      title: 'API Documentation iTicket project',
-      version: '1.0.0',
-      description: 'Documentation for the backend APIs',
+      title: "API Documentation for iTicket Project",
+      version: "1.0.0",
+      description: "Documentation for the backend APIs",
     },
-    basePath: '/api/v1',
     servers: [
       {
-        url: `http://localhost:${PORT}`,
+        url: "https://abdulaziz-test.onrender.com", // Твой продакшн-домен
       },
     ],
     components: {
       securitySchemes: {
         BearerAuth: {
-          type: 'http',
-          scheme: 'bearer',
-          bearerFormat: 'JWT',
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
         },
       },
     },
@@ -32,7 +29,7 @@ const swaggerOptions = {
       },
     ],
   },
-  apis: [path.join(__dirname, '/routes/*.js')],
+  apis: [path.join(__dirname, "routes/**/*.js")], // Подключает все файлы API в routes/
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
